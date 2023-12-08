355218d098aab7311fe3ae5a5ef76933d3170a20
Yevheniia Kurtasova <eugenia.kurtasova@gmail.com>
2023-12-08 22:04:44 +0200

    create elements from array

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './assets/Logo2.png'
 import './App.css';
 
+
 function App() {
+  const users = [
+    {name: '1', lastName: '11', number: "01"},
+    {name: '2', lastName: '22', number: "02"},
+    {name: '3', lastName: '33', number: "03"},
+    {name: '4', lastName: '44', number: "04"}
+  ]
+  
   return (
     <>
-      <h1>Hello world</h1>
-      <img src={logo} alt="logo"/>
-      <h1>Hello React</h1>
+      {users.map(({ name, lastName, number }) => {
+        return (
+          <> 
+          <h3>{name}</h3>
+          <h3>{lastName}</h3>
+          <h3>{number}</h3>
+          </>
+        )
+      }) }
     </>
   );
 }
