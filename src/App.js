af1b945d1de06c81895a6386bc91008762d4ee4c
Yevheniia Kurtasova <eugenia.kurtasova@gmail.com>
2023-12-08 22:39:51 +0200

    add page with paragraph and img

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
 import './App.css';
-
+import logo from './assets/cat_img.jpg'
 
 function App() {
-  const users = [
-    {name: '1', lastName: '11', number: "01"},
-    {name: '2', lastName: '22', number: "02"},
-    {name: '3', lastName: '33', number: "03"},
-    {name: '4', lastName: '44', number: "04"}
-  ]
   
-  return (
-    <>
-      {users.map(({ name, lastName, number }) => {
         return (
           <> 
-          <h3>{name}</h3>
-          <h3>{lastName}</h3>
-          <h3>{number}</h3>
+          <h2 style={{fontFamily: 'Tahoma'}}>{'Task 2'}</h2>
+          <p style={{fontFamily: 'Tahoma', color: 'darkgreen', backgroundColor: 'yellow', fontSize: '20px'}}>Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.
+            </p>
+            <img src={logo} alt="cat img"></img>
+            <> 
+            <form>
+              <label htmlFor>HTML</label>
+              <input type="text" name="html"></input>
+            </form>
+            </>
           </>
         )
-      }) }
-    </>
-  );
 }
 
 export default App;
