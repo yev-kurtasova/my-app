bd48722cccc5430dc19b7df878e5bd3a6a7ac3f1
Yevheniia Kurtasova <eugenia.kurtasova@gmail.com>
2023-12-08 23:24:47 +0200

    add page by createElement()

--- src/App.js
+++ src/App.js
@@ -1,22 +1,26 @@
-import './App.css';
-import logo from './assets/cat_img.jpg'
+import logo from './assets/cat_img.jpg';
+import React from 'react';
+import './App.css'
 
 function App() {
-  
-        return (
-          <> 
-          <h2 style={{fontFamily: 'Tahoma'}}>{'Task 2'}</h2>
-          <p style={{fontFamily: 'Tahoma', color: 'darkgreen', backgroundColor: 'yellow', fontSize: '20px'}}>Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.
-            </p>
-            <img src={logo} alt="cat img"></img>
-            <> 
-            <form>
-              <label htmlFor>HTML</label>
-              <input type="text" name="html"></input>
-            </form>
-            </>
-          </>
-        )
+
+  const element1 = React.createElement('h1', { 'fontFamily': 'Tahoma' }, 'Task 2')
+
+  const element = React.createElement('p', { 'fontFamily': 'Tahoma', 'color': 'darkgreen', 'backgroundColor': 'yellow', 'fontSize': '20px' }, 'Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.');
+  return (
+    <>
+      <h2 style={{ fontFamily: 'Tahoma' }}>{'Task 2'}</h2>
+      <p style={{ fontFamily: 'Tahoma', color: 'darkgreen', backgroundColor: 'yellow', fontSize: '20px' }}>Розробити сторінку з заголовком, параграфом тексту та зображенням за допомогою синтаксису JSX. Повторити дану сторінку, використовуючи метод createElement.
+      </p>
+      <img src={logo} alt="cat img"></img>
+      {/* <form>
+        <label htmlFor>HTML</label>
+        <input type="text" name="html"></input>
+      </form> */}
+      {element1}
+      {element}
+    </>
+  )
 }
 
 export default App;
